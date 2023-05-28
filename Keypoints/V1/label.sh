7b283546741afc834e9532e89f0fdf341520e7fc
arch <arch@local>
2023-05-28 17:21:11 +0200

    improve label tool

--- Keypoints/V1/label.sh
+++ Keypoints/V1/label.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-nix develop --command python3 label.py ./data/videos/example2.mkv
+for f in ./data/videos/*.mkv; do
+    echo $f
+    nix develop --command python3 label.py $f
+done
+for f in ./data/videos/*.mp4; do
+    echo $f
+    nix develop --command python3 label.py $f
+done
